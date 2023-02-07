ab27361bec481224ffa65ba9e4c0ac5e51889338
Hellen <hellenn.s@outlook.com>
2023-02-07 15:16:35 -0300

    correcao exercicio

--- bhaskara.c
+++ bhaskara.c
@@ -3,32 +3,33 @@
 
 int main()
 {
-    int a;
-    int b;
-    int c;
+    float a;
+    float b;
+    float c;
     float delta;
     float x1;
     float x2;
 
     printf("Coeficiente a: ");
-    scanf("%d", &a);
+    scanf("%f", &a);
 
     printf("Coeficiente b: ");
-    scanf("%d", &b);
+    scanf("%f", &b);
 
     printf("Coeficiente c: ");
-    scanf("%d", &c);
+    scanf("%f", &c);
 
-    delta = pow(b,2) - (4 *a *c);
+    delta = pow(b,2) - (4 * a * c);
 
-    if (delta = 0)
+    if (delta <= 0)
     {
         printf("Esta equacao nao possui raizes reais");
     }
-   else {
-    x1 = (- b + sqrt(delta)) / (2 * a);
-    x2 = (-b - sqrt(delta)) / (2 * a);
-    printf("X1 = %.4f\n", x1);
-    printf("X2 = %.4f", x2);
-   }
+    else
+    {
+        x1 = (-b + sqrt(delta)) / (2 * a);
+        x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("X1 = %.4f\n", x1);
+        printf("X2 = %.4f", x2);
+    }
 }
